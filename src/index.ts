5842627250e6135c83b92620bb51564f0f91dc7a
Bung Danar <danar.g.putera@gmail.com>
2023-12-15 17:15:18 +0700

    chore: Add openapi parser package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-const app = () => {
-  console.log('Hello World')
+import OpenApiParser from '@readme/openapi-parser'
+
+const app = async () => {
+  try {
+    const apiSpec = await OpenApiParser.validate('openapi.example.json')
+    console.log(
+      `API name: ${apiSpec.info.title}, Version: ${apiSpec.info.version}`
+    )
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 app()
